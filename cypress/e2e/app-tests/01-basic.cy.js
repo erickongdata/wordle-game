005028574d2250876ed335a43d8f296a9a6c5279
Eric Kong <erickongdata@gmail.com>
2022-07-26 12:54:43 +0100

    Cypress Testing install

---
+++ cypress/e2e/app-tests/01-basic.cy.js
@@ -0,0 +1,124 @@
+/// <reference types="cypress" />
+
+describe('page renders correctly', () => {
+  before(() => {
+    cy.visit('http://localhost:5173/');
+  });
+  it('page renders all components', () => {
+    cy.contains('Wordle').should('exist');
+    cy.get('[data-id="custom-word-btn"]').should('exist');
+    cy.get('[data-id="mode-btn"]').should('exist');
+    cy.get('[data-id="help-btn"]').should('exist');
+    cy.get('[data-id="quit-btn"]').should('exist');
+    cy.get('[data-id="tile-0-0"]').should('exist');
+    cy.get('[data-id="tile-5-4"]').should('exist');
+    cy.get('.tile').should('have.length', 30);
+    cy.get('.key-tile').should('have.length', 28);
+    cy.get('[data-key="Q"]').should('exist');
+    cy.get('[data-key="M"]').should('exist');
+    cy.get('[data-key="Enter"]').should('exist');
+    cy.get('[data-key="Backspace"]').should('exist');
+  });
+
+  it('quit button is not visible', () => {
+    cy.get('[data-id="quit-btn"]').should('have.css', 'display', 'none');
+    cy.get('[data-id="quit-btn"]').should('not.be.visible');
+  });
+
+  it('grid is blank', () => {
+    cy.get('[data-id="tile-0-0"]').should('have.text', '');
+    cy.get('[data-id="tile-5-4"]').should('have.text', '');
+
+    cy.get('.absent').should('not.exist');
+    cy.get('.present').should('not.exist');
+    cy.get('.correct').should('not.exist');
+    cy.get('.flipped-correct').should('not.exist');
+  });
+});
+
+describe('menu buttons work', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('custom word button works', () => {
+    cy.get('[data-id="word-container"]').should('not.be.visible');
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-container"]').should('be.visible');
+    cy.get('[data-id="word-close-btn"]').click();
+    cy.get('[data-id="word-container"]').should('not.be.visible');
+  });
+
+  it('help button works', () => {
+    cy.get('[data-id="help-container"]').should('not.be.visible');
+    cy.get('[data-id="help-btn"]').click();
+    cy.get('[data-id="help-container"]').should('be.visible');
+    cy.get('[data-id="help-close-btn"]').click();
+    cy.get('[data-id="help-container"]').should('not.be.visible');
+  });
+
+  it('mode button works', () => {
+    cy.get('[data-id="mode-btn"]').should('contain.text', '✭');
+    cy.get('[data-id="mode-btn"]').should('not.contain.text', '✭✭');
+    cy.get('[data-id="mode-btn"]').click();
+    cy.get('[data-id="mode-btn"]').should('contain.text', '✭✭');
+    cy.get('[data-id="mode-btn"]').should('not.contain.text', '✭✭✭');
+    cy.get('[data-id="mode-btn"]').click();
+    cy.get('[data-id="mode-btn"]').should('contain.text', '✭✭✭');
+    cy.get('[data-id="mode-btn"]').click();
+    cy.get('[data-id="mode-btn"]').should('contain.text', '✭');
+    cy.get('[data-id="mode-btn"]').should('not.contain.text', '✭✭');
+  });
+});
+
+describe('Key input and keyboard works', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+  });
+
+  it('physical key input works', () => {
+    cy.get('body').type('s');
+    cy.get('[data-id="tile-0-0"]').should('have.text', 'S');
+    cy.get('body').type('p');
+    cy.get('[data-id="tile-0-1"]').should('have.text', 'P');
+    cy.get('body').type('e');
+    cy.get('[data-id="tile-0-2"]').should('have.text', 'E');
+    cy.get('body').type('a');
+    cy.get('[data-id="tile-0-3"]').should('have.text', 'A');
+    cy.get('body').type('k');
+    cy.get('[data-id="tile-0-4"]').should('have.text', 'K');
+    cy.get('body').type('{backspace}');
+    cy.get('[data-id="tile-0-4"]').should('have.text', '');
+    cy.get('body').type('{backspace}');
+    cy.get('[data-id="tile-0-3"]').should('have.text', '');
+    cy.get('body').type('{backspace}');
+    cy.get('[data-id="tile-0-2"]').should('have.text', '');
+    cy.get('body').type('{backspace}');
+    cy.get('[data-id="tile-0-1"]').should('have.text', '');
+    cy.get('body').type('{backspace}');
+    cy.get('[data-id="tile-0-0"]').should('have.text', '');
+  });
+
+  it('virtual keyboard input works', () => {
+    cy.get('[data-key="S"]').click();
+    cy.get('[data-id="tile-0-0"]').should('have.text', 'S');
+    cy.get('[data-key="P"]').click();
+    cy.get('[data-id="tile-0-1"]').should('have.text', 'P');
+    cy.get('[data-key="E"]').click();
+    cy.get('[data-id="tile-0-2"]').should('have.text', 'E');
+    cy.get('[data-key="A"]').click();
+    cy.get('[data-id="tile-0-3"]').should('have.text', 'A');
+    cy.get('[data-key="K"]').click();
+    cy.get('[data-id="tile-0-4"]').should('have.text', 'K');
+    cy.get('[data-key="Backspace"]').click();
+    cy.get('[data-id="tile-0-4"]').should('have.text', '');
+    cy.get('[data-key="Backspace"]').click();
+    cy.get('[data-id="tile-0-3"]').should('have.text', '');
+    cy.get('[data-key="Backspace"]').click();
+    cy.get('[data-id="tile-0-2"]').should('have.text', '');
+    cy.get('[data-key="Backspace"]').click();
+    cy.get('[data-id="tile-0-1"]').should('have.text', '');
+    cy.get('[data-key="Backspace"]').click();
+    cy.get('[data-id="tile-0-0"]').should('have.text', '');
+  });
+});
---
+++ cypress/e2e/app-tests/02-gameLogic.cy.js
@@ -0,0 +1,169 @@
+/// <reference types="cypress" />
+
+describe('basic game logic check', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:5173/');
+    cy.intercept('https://api.dictionaryapi.dev/api/v2/entries/en/**', {}).as(
+      'dictAPI'
+    );
+  });
+
+  it('custom word input works', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]')
+      .type('baker')
+      .should('have.value', 'baker')
+      .type('{enter}')
+      .should('not.be.visible');
+    cy.get('body').type('b');
+    cy.get('[data-id="tile-0-0"]').should('have.text', 'B');
+    cy.get('body').type('{a}{k}{e}{r}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-0"]').should('not.have.class', 'present');
+    cy.get('[data-id="tile-0-0"]').should('not.have.class', 'absent');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-1"]').should('not.have.class', 'present');
+    cy.get('[data-id="tile-0-1"]').should('not.have.class', 'absent');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-2"]').should('not.have.class', 'present');
+    cy.get('[data-id="tile-0-2"]').should('not.have.class', 'absent');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-3"]').should('not.have.class', 'present');
+    cy.get('[data-id="tile-0-3"]').should('not.have.class', 'absent');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-4"]').should('not.have.class', 'present');
+    cy.get('[data-id="tile-0-4"]').should('not.have.class', 'absent');
+
+    cy.get('[data-key="B"]').should('have.class', 'correct');
+    cy.get('[data-key="A"]').should('have.class', 'correct');
+    cy.get('[data-key="K"]').should('have.class', 'correct');
+    cy.get('[data-key="E"]').should('have.class', 'correct');
+    cy.get('[data-key="R"]').should('have.class', 'correct');
+  });
+
+  it('wrong letters show dark tiles', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('baker{enter}');
+    cy.get('body').type('{c}{o}{l}{d}{s}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'absent');
+
+    cy.get('[data-key="C"]').should('have.class', 'absent');
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="L"]').should('have.class', 'absent');
+    cy.get('[data-key="D"]').should('have.class', 'absent');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+  });
+
+  it('present letters show up', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('baker{enter}');
+    cy.get('body').type('{c}{o}{o}{k}{s}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'present');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'absent');
+
+    cy.get('[data-key="C"]').should('have.class', 'absent');
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="K"]').should('have.class', 'present');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+  });
+
+  it('correct letters show up', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('baker{enter}');
+    cy.get('body').type('{b}{o}{o}{k}{s}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'present');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'absent');
+
+    cy.get('[data-key="B"]').should('have.class', 'correct');
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="K"]').should('have.class', 'present');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+  });
+
+  it('double present letters show up', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('books{enter}');
+    cy.get('body').type('{i}{g}{l}{o}{o}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'present');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'present');
+
+    cy.get('[data-key="I"]').should('have.class', 'absent');
+    cy.get('[data-key="G"]').should('have.class', 'absent');
+    cy.get('[data-key="L"]').should('have.class', 'absent');
+    cy.get('[data-key="O"]').should('have.class', 'present');
+  });
+
+  it('double correct letters show up', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('books{enter}');
+    cy.get('body').type('{c}{o}{o}{l}{s}{enter}');
+    cy.get('[data-id="tile-0-0"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-1"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-2"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-0-3"]').should('have.class', 'absent');
+    cy.get('[data-id="tile-0-4"]').should('have.class', 'correct');
+
+    cy.get('[data-key="C"]').should('have.class', 'absent');
+    cy.get('[data-key="O"]').should('have.class', 'correct');
+    cy.get('[data-key="L"]').should('have.class', 'absent');
+    cy.get('[data-key="S"]').should('have.class', 'correct');
+  });
+
+  it.only('correct/present tiles accumulate on next row', () => {
+    cy.get('[data-id="custom-word-btn"]').click();
+    cy.get('[data-id="word-input"]').type('baker{enter}');
+    cy.get('body').type('{b}{o}{o}{k}{s}{enter}');
+    cy.get('body').type('{b}{a}{r}{k}{s}{enter}');
+    cy.get('[data-id="tile-1-0"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-1-1"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-1-2"]').should('have.class', 'present');
+    cy.get('[data-id="tile-1-3"]').should('have.class', 'present');
+    cy.get('[data-id="tile-1-4"]').should('have.class', 'absent');
+
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="B"]').should('have.class', 'correct');
+    cy.get('[data-key="A"]').should('have.class', 'correct');
+    cy.get('[data-key="R"]').should('have.class', 'present');
+    cy.get('[data-key="K"]').should('have.class', 'present');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+
+    cy.get('body').type('{b}{a}{k}{e}{s}{enter}');
+    cy.get('[data-id="tile-2-0"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-2-1"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-2-2"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-2-3"]').should('have.class', 'correct');
+    cy.get('[data-id="tile-2-4"]').should('have.class', 'absent');
+
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="B"]').should('have.class', 'correct');
+    cy.get('[data-key="A"]').should('have.class', 'correct');
+    cy.get('[data-key="R"]').should('have.class', 'present');
+    cy.get('[data-key="K"]').should('have.class', 'correct');
+    cy.get('[data-key="E"]').should('have.class', 'correct');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+
+    cy.get('body').type('{b}{a}{k}{e}{r}{enter}');
+
+    cy.get('[data-key="O"]').should('have.class', 'absent');
+    cy.get('[data-key="B"]').should('have.class', 'correct');
+    cy.get('[data-key="A"]').should('have.class', 'correct');
+    cy.get('[data-key="R"]').should('have.class', 'correct');
+    cy.get('[data-key="K"]').should('have.class', 'correct');
+    cy.get('[data-key="E"]').should('have.class', 'correct');
+    cy.get('[data-key="S"]').should('have.class', 'absent');
+  });
+});
